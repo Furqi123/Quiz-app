7490bce925cf26cb9c37d9eb5cf6f08d1f84fe6d
furqan <furqan.azeem369@gmail.com>
2023-10-30 16:00:07 +0500

    code added

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var htmlQuestions = [
     answer: 'target'
   },
 ]
-/*var cssQuestion = [
+var cssQuestion = [
     {
         question: 'What does CSS stand for?',
         option1: 'Cascade sheets style',
@@ -117,14 +117,6 @@ var htmlQuestions = [
     },
     {
         question: 'Which below property of CSS is used to set the indentation of the first line in a block of text ?',
-        option1: '',
-        option2: '< img >',     
-        option3: '< div >',
-        option4: '< link >',
-        answer: '< a >'
-    },
-    {
-        question: 'Which tag is used to create a hyperlink?',
         option1: '< a >',
         option2: '< img >',     
         option3: '< div >',
@@ -140,28 +132,36 @@ var htmlQuestions = [
         answer: '< a >'
     },
     {
-        question: 'Which tag is used to create a hyperlink?',
-        option1: '< a >',
-        option2: '< img >',     
-        option3: '< div >',
-        option4: '< link >',
-        answer: '< a >'
+        question: 'Which of the following is the correct syntax to add the external stylesheet in CSS?',
+        option1: '<style src = quiz.css >',
+        option2: '<style src = "quiz.css" >',     
+        option3: '< stylesheet > quiz.css < /stylesheet >',
+        option4: '< link rel="stylesheet" type="quiz/css" href=" quiz.css " >',
+         answer: '< link rel="stylesheet" type="quiz/css" href="quiz.css" >'
     },
     {
-        question: 'Which tag is used to create a hyperlink?',
-        option1: '< a >',
-        option2: '< img >',     
-        option3: '< div >',
-        option4: '< link >',
-        answer: '< a >'
+        question: 'Which below property of CSS is used to set the indentation of the first line in a block of text ',
+        option1: 'text-indent property',
+        option2: 'text-underlne-property',     
+        option3: 'text-decoration none',
+        option4: 'text-overflow property',
+        answer: 'text-indent property'
     },
     {
-        question: 'Which tag is used to create a hyperlink?',
-        option1: '< a >',
-        option2: '< img >',     
-        option3: '< div >',
-        option4: '< link >',
-        answer: '< a >'
+        question: 'Which of the below CSS properties represent the order of flex items in the grid container ?',
+        option1: 'order',
+        option2: 'float',     
+        option3: 'overflow',
+        option4: 'All of the above',
+        answer: 'order'
+    },
+    {
+        question: 'How do we set the default width of the font in CSS ?',
+        option1: 'font-stretch',
+        option2: 'font-weight',     
+        option3: 'text-transform',
+        option4: 'font-variant',
+        answer: 'font-stretch'
     },
 ] 
 
@@ -246,112 +246,39 @@ var jsQuestion = [
             option4: '< link >',
             answer: '< a >'
         },
-    ] */
+    ] 
 
 
 
-function loginPage() {
-  var main = document.getElementById('main')
-  main.innerHTML = `
-    <div id="sign-up" class="sign-up">
-      <div class="sign-up-title">
-        <p>Login Form</p>
-      </div>
-      <div class="sign-up-body">
-        <div class="sign-up-email">
-          <label for="">Email Address</label>
-          <input type="text" name="" id="email">
-        </div>
-
-        <div class="sign-up-pass">
-          <label for="">Password</label>
-          <input type="text" name="" id="">
-          <p>Forget Password</p>
-        </div>
-
-        <div class="sign-up-btn">
-          <button onclick="quizSelect()">Login</button>
-        </div>
-
-      </div>
-
-    </div>
-    `
-}
-
-loginPage();
+function quizSelect(){
 var email = document.getElementById('email')
 var pass = document.getElementById('pass')
-function quizSelect() {
+var signUpForm = document.getElementById('sign-up')
+var quiz_select = document.getElementById('quiz-select')
 
-  var main = document.getElementById('main')
-  main.innerHTML = `
-    <div class="quiz-selection" id="quiz-select">
-    <div class="row">
-      <div class="quiz-box col-4">
-        <div class="animate__animated animate__fadeInDown card">
-          <div class="card-body">
-          <h2>HTML</h2>
-          <p>Each Question Time: 20sec</p>
-          <p>Total Question: 10</p>
-          <p>Each Question: 10 Marks</p>
-          <p>Level: Basic</p>
-          <button onclick="pressHtml()" class="box-btn">Start Quiz</button>
-          </div>
-         </div>
-      </div>
-
-      <div class="quiz-box col-4">
-        <div onclick = "press()" class="animate__animated animate__fadeInDown card">
-          <div class="card-body">
-          <h2>CSS</h2>
-          <p>Each Question Time: 20sec</p>
-          <p>Total Question: 10</p>
-          <p>Each Question: 10 Marks</p>
-          <p>Level: Basic</p>
-          <button class="box-btn">Start Quiz</button>
-          </div>
-         </div>
-      </div>
-
-      <div class="quiz-box col-4">
-        <div onclick = "press()" class="animate__animated animate__fadeInDown card">
-          <div class="card-body">
-          <h2>Javascript</h2>
-          <p>Each Question Time: 20sec</p>
-          <p>Total Question: 10</p>
-          <p>Each Question: 10 Marks</p>
-          <p>Level: Basic</p>
-          <button class="box-btn">Start Quiz</button>
-         
-          </div>
-         </div>
-      </div>
-    </div>
-  </div> 
-    `
+if(email.value == "furi" && pass.value == 2567){
+  signUpForm.style.display = 'none';
+   quiz_select.style.display = 'block';
+}
+else{
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Something went wrong!',
+    footer: '<a href="">Why do I have this issue?</a>'
+  })
+}
+email.value = ""
+pass.value = ""
 }
 
-var htmlIndex = 0;
-var correctCount = 0;
+var htmlIndex = 0
+var correctCount = 0
 var quesCount = 1
-var score = 0;
-/*var sec = 20;
-var timer = document.getElementById('time')
-
-var interval = setInterval(function () {
-  timer.innerHTML = `Timer: ${sec}`
-  sec--;
-  if (sec == 0) {
-    htmlIndex++;
-    sec = 20;
-  }
-}, 1000)
-*/
-
-
-function pressHtml() {
-  var main = document.getElementById('main');
+var score = 0
+var sec = 20
+function nextQuestionHtml(){
+  var cont = document.getElementById('cont')
   var options = document.getElementsByName('option');
   for (var i = 0; i < options.length; i++) {
     if (options[i].checked) {
@@ -363,9 +290,12 @@ function pressHtml() {
     }
 
   }
+
+   
   if (!htmlQuestions[htmlIndex]) {
-    console.log((score / 10) * 100)
-    main.innerHTML = `
+   (score / 10) * 100
+
+    cont.innerHTML = `
     <div class="result">
     <div class="result-icon">
        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
@@ -386,59 +316,175 @@ function pressHtml() {
       
       <div class="result-btn">
        
-      <button onclick="loginPage()">Quit Quiz</button>
+      <a href = "./index.html"><button>Quit Quiz</button></a>
      
       </div>
      
       </div>
     `
+   console.log(htmlIndex)
     return;
   }
-  else {
-    main.innerHTML = `
-    <div class="box" id="quiz-question">
-      <div class="correct-timer">
-        <div class="count">
-          <p>Correct: <span class="comb-color">${correctCount}/10</span></p>
-        </div>
-        <div id="time" class="time">
-       
-        </div>
-        </div>
-      <br>
-      <div class="box-body">
-      
-      <p class="ques">${quesCount++}) ${htmlQuestions[htmlIndex].question}</p>
-        <div>
-        <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option1}" id="">${htmlQuestions[htmlIndex].option1}</label>
-        </div>
-        <br>
-        <div>
-        <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option2}" id="">${htmlQuestions[htmlIndex].option2}</label>
-        </div>
-        <br>
-        <div>
-          <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option3}" id="">${htmlQuestions[htmlIndex].option3}</label>
-        </div>
-        <br>
-        <div>
-          <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option4}" id="">${htmlQuestions[htmlIndex].option4}</label>
-          </div>
-          <div>
-          <button disabled id="btn" onclick="pressHtml()" class="box-btn">Next</button>
-        </div>
-        </div>
-    </div>
-    `
 
-    htmlIndex++;
-  }
+
+  cont.innerHTML = `
+  <div class="box" id="quiz-question">
+  <div class="correct-timer">
+    <div class="count">
+      <p>Correct: <span class="comb-color">${correctCount}/10</span></p>
+    </div>
+    <div class="time">
+      <p>Timer: <span class="comb-color">20</span></p>
+    </div>
+  </div>
+  <br>
+  <div class="box-body">
+
+    <p class="ques">${quesCount++}) ${htmlQuestions[htmlIndex].question}</p>
+    <div>
+      <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option1}" id="">${htmlQuestions[htmlIndex].option1}</label>
+    </div>
+    <br>
+    <div>
+      <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option2}" id="">${htmlQuestions[htmlIndex].option2}</label>
+    </div>
+    <br>
+    <div>
+      <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option3}" id="">${htmlQuestions[htmlIndex].option3}</label>
+    </div>
+    <br>
+    <div>
+      <label for=""><input onclick="clicked()" type="radio" name="option" value="${htmlQuestions[htmlIndex].option4}" id="">${htmlQuestions[htmlIndex].option4}</label>
+    </div>
+    <div>
+      <button disabled onclick="nextQuestionHtml()" id="btn" class="box-btn">Next</button>
+    </div>
+  </div>
+</div> 
+  `
+  htmlIndex++;
+
+ 
 }
 
-
-
-
+nextQuestionHtml()
+ 
 function clicked() {
   var btn = document.getElementById('btn')
   btn.disabled = false
 }
+
+
+
+
+// var cssIndex = 0
+// var correctCountCss = 0
+// var quesCountCss = 1
+// var scoreCss = 0
+// var sec = 20
+// function nextQuestionCss(){
+ 
+//     var cont = document.getElementById('cont')
+//     var options = document.getElementsByName('option');
+//     for (var i = 0; i < options.length; i++) {
+//       if (options[i].checked) {
+//         console.log(options[i].value)
+//         if (options[i].value === cssQuestion[cssIndex - 1].answer) {
+//           scoreCss += 10
+//           correctCountCss++
+//         }
+//       }
+  
+//     }
+  
+     
+//     if (!cssQuestion[cssIndex]) {
+//      (scoreCss / 10) * 100
+  
+//       cont.innerHTML = `
+//       <div class="result">
+//       <div class="result-icon">
+//          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+//            <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
+//          </svg>
+//         </div>
+//         <br>
+//         <div class="result-body">
+//          <p>Congratulation</p>
+        
+//          </div>
+//         <div class="result-body-p2">
+//         <p>You Have Get</p>
+//         </div>
+//         <div class="result-body-p3">
+//          <p> ${scoreCss}% Percentage</p>
+//         </div>
+        
+//         <div class="result-btn">
+         
+//         <a href = "./index.html"><button>Quit Quiz</button></a>
+       
+//         </div>
+       
+//         </div>
+//       `
+  
+//       return;
+//     }
+  
+  
+//     cont.innerHTML = `
+//     <div class="box" id="quiz-question">
+//     <div class="correct-timer">
+//       <div class="count">
+//         <p>Correct: <span class="comb-color">${correctCountCss}/10</span></p>
+//       </div>
+//       <div class="time">
+//         <p>Timer: <span class="comb-color">20</span></p>
+//       </div>
+//     </div>
+//     <br>
+//     <div class="box-body">
+  
+//       <p class="ques">${quesCountCss++}) ${cssQuestion[cssIndex].question}</p>
+//       <div>
+//         <label for=""><input onclick="clicked()" type="radio" name="option" value="${cssQuestion[cssIndex].option1}" id="">${cssQuestion[cssIndex].option1}</label>
+//       </div>
+//       <br>
+//       <div>
+//         <label for=""><input onclick="clicked()" type="radio" name="option" value="${cssQuestion[cssIndex].option2}" id="">${cssQuestion[cssIndex].option2}</label>
+//       </div>
+//       <br>
+//       <div>
+//         <label for=""><input onclick="clicked()" type="radio" name="option" value="${cssQuestion[cssIndex].option3}" id="">${cssQuestion[cssIndex].option3}</label>
+//       </div>
+//       <br>
+//       <div>
+//         <label for=""><input onclick="clicked()" type="radio" name="option" value="${cssQuestion[cssIndex].option4}" id="">${cssQuestion[cssIndex].option4}</label>
+//       </div>
+//       <div>
+//         <button disabled onclick="nextQuestionCss()" id="btn" class="box-btn">Next</button>
+//       </div>
+//     </div>
+//   </div> 
+//     `
+//     cssIndex++;
+  
+   
+//   }
+   
+// nextQuestionCss()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
